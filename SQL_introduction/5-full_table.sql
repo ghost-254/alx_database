618a8f5b34e614fba1d3dd9a2654d3a8fe294cb9
ghost-254 <nevilleotieno6@gmail.com>
2023-09-09 05:00:46 -0700

    5. Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,16 @@
 -- Retrieves the full description of the "first_table" from the "hbtn_0c_0" database in MySQL
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
+SELECT CONCAT('CREATE TABLE `first_table` (',
+    GROUP_CONCAT(
+        CONCAT('`', COLUMN_NAME, '` ', DATA_TYPE,
+            IF(CHARACTER_MAXIMUM_LENGTH IS NOT NULL,
+               CONCAT('(', CHARACTER_MAXIMUM_LENGTH, ')'), ''),
+            IF(IS_NULLABLE = 'NO', ' NOT NULL', ''),
+            IF(COLUMN_DEFAULT IS NOT NULL,
+               CONCAT(' DEFAULT ', COLUMN_DEFAULT), ''),
+            IF(EXTRA = 'auto_increment', ' AUTO_INCREMENT', '')
+        )
+    ),
+    ', PRIMARY KEY (`id`)'
+) AS CreateTableSQL
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = 'first_table' AND TABLE_SCHEMA = 'hbtn_0c_0';
