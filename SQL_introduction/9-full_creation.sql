8a50a37c34da99d14601d265ef577453fa3cb2e3
ghost-254 <nevilleotieno6@gmail.com>
2023-09-09 05:13:00 -0700

    9. Full creation

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Create table second_table if it doesn't exist
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+-- Insert records into second_table
+INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
