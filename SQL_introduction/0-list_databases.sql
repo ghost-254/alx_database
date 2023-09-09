fcdae273f290dfaa9b19eff640318d2c0b169bc2
ghost-254 <nevilleotieno6@gmail.com>
2023-09-09 04:11:21 -0700

    0. List databases

---
+++ SQL_introduction/0-list_databases.sql
@@ -0,0 +1,38 @@
+import mysql.connector
+
+# Replace these with your MySQL server details
+host = 1e456a5f2ef0.dd860876.alx-cod.online
+user = 1e456a5f2ef0
+password = 523e081667aab6198723
+
+try:
+    # Connect to the MySQL server
+    connection = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password
+    )
+
+    # Create a cursor object to execute SQL commands
+    cursor = connection.cursor()
+
+    # Execute the SHOW DATABASES statement
+    cursor.execute("SHOW DATABASES")
+
+    # Fetch all the rows (databases)
+    databases = cursor.fetchall()
+
+    # Print the list of databases
+    print("List of databases:")
+    for db in databases:
+        print(db[0])
+
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+
+finally:
+    # Close the cursor and connection
+    if cursor:
+        cursor.close()
+    if connection:
+        connection.close()
