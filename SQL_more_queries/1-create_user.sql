0b45e1529e65cea8697e293801f3c8ae9eabcd58
ghost-254 <nevilleotieno6@gmail.com>
2023-09-17 22:51:25 -0700

    1. Root user

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
